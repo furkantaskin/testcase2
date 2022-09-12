336c819bb91d4499ce620edb44bf33bee8a42814
Furkan Taşkın <furkan.taskin@tuta.io>
2022-09-12 23:04:21 +0300

    Initial commit for files

---
+++ app.js
@@ -0,0 +1,32 @@
+const API_URL = "https://restcountries.com/v3.1/all";
+
+
+async function fetchData() {
+  const response = await fetch(API_URL);
+  const countries = await response.json();
+  return countries;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  let table = $("#countries").DataTable({
+  dom: "Bfrtip",
+  buttons: ["copyHtml5", "excelHtml5", "csvHtml5", "pdfHtml5"],
+});
+  fetchData()
+    .then((aggregatedData) => {
+      aggregatedData.map((country) => {
+        table.row
+          .add([
+            `<img src='${country.flags.png}' class='w-10 h-10' />`,
+            country.name.common,
+            country.capital.toLocaleString(),
+            country.population.toLocaleString(),
+            country.region.toLocaleString()
+          ])
+          .draw();
+      });
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+});
