9f3d98908e807a1e89e9e02a14b16186fb560cd8
Furkan Taşkın <furkan.taskin@tuta.io>
2022-09-12 23:35:54 +0300

    Fix toLocaleString() error

--- app.js
+++ app.js
@@ -9,19 +9,19 @@ async function fetchData() {
 
 document.addEventListener("DOMContentLoaded", function () {
   let table = $("#countries").DataTable({
-  dom: "Bfrtip",
-  buttons: ["copyHtml5", "excelHtml5", "csvHtml5", "pdfHtml5"],
-});
+    dom: "Bfrtip",
+    buttons: ["copyHtml5", "excelHtml5", "csvHtml5", "pdfHtml5"],
+  });
   fetchData()
     .then((aggregatedData) => {
       aggregatedData.map((country) => {
         table.row
           .add([
-            `<img src='${country.flags.png}' class='w-10 h-10' />`,
+            `<img src='${country.flags.png}' class='w-10 h-10 object-contain object-center' />`,
             country.name.common,
-            country.capital.toLocaleString(),
-            country.population.toLocaleString(),
-            country.region.toLocaleString()
+            country.capital?.toLocaleString('en-US') || '',
+            country.population?.toLocaleString('en-US') || '',
+            country.region?.toLocaleString('en-US') || '',
           ])
           .draw();
       });
